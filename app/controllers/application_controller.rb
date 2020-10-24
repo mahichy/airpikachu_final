2594362ca68c38fe20e82553b9d3d4ad9c3cfa7b
mahi <mohiddinoshim@yahoo.com>
2020-10-24 23:03:54 +0600

    Update authentication View

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 	protected
 
 	def configure_permitted_parameters
-		devise_perameter_sanitizer.permit(:sign_up, keys: [:fullname]) 	
-		devise_perameter_sanitizer.permit(:account_update, keys: [:fullname]) 	
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:fullname]) 	
+		devise_parameter_sanitizer.permit(:account_update, keys: [:fullname]) 	
 	end 
 end
